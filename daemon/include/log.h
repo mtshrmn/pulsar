25f731ff8db2c1d0d7c2f01e2857a1b5f6b34a77
Moshe Sherman <mtshrmn@gmail.com>
2025-06-06 03:16:07 +0300

    add file and func name to logs

--- daemon/include/log.h
+++ daemon/include/log.h
@@ -5,8 +5,10 @@
 
 #ifdef LOGLEVEL_INFO
 #define LOGE(fmt, ...)                                                         \
-  printf("[\033[0;31mERROR\033[0m]: " fmt "\n", ##__VA_ARGS__)
-#define LOGI(fmt, ...) printf("[INFO]: " fmt "\n", ##__VA_ARGS__)
+  printf("[\033[0;31mERROR\033[0m][%s:%s] :: " fmt "\n", __FILE__, __func__,   \
+         ##__VA_ARGS__)
+#define LOGI(fmt, ...)                                                         \
+  printf("[INFO][%s:%s] :: " fmt "\n", __FILE__, __func__, ##__VA_ARGS__)
 #endif
 
 #ifdef LOGLEVEL_ERROR
